b6c2521b109dff00bc583f35190ff4126d975f1c
aleksanderkaasik <aleks.kaasik@outlook.com>
2026-03-27 16:05:25 +0200

    Spliting the main variable file to multiple files. Moving some task to diffrent playbook. and modify task.

--- main.sh
+++ main.sh
@@ -1,23 +1,25 @@
 ansible-playbook lxc-setup.yml -u root
-ansible-playbook mysql.yml
-ansible-playbook pki-server.yml
-ansible-playbook importing-ca-crt.yml
 ansible-playbook bind9.yml
 
+ansible all -m reboot --become
+
+ansible-playbook mysql.yml
+
+ansible-playbook pki-server.yml
+
+ansible-playbook importing-ca-crt.yml
 
 ansible-playbook vaultwarden.yml
 ansible-playbook wordpress.yml
 ansible-playbook nextcloud.yml
 
-ansible-playbook Pterodactyl-panel.yml
-# ansible-playbook Pterodactyl-wing.yml
-
+ansible-playbook pterodactyl-panel.yml
+# ansible-playbook pterodactyl-wing.yml
 
 ansible-playbook zabbix-server.yml
-ansible-playbook zabbix-agent.yml
+#ansible-playbook zabbix-agent.yml
 
-ansible-playbook snmp.yml
+#ansible-playbook snmp.yml
 
-
-ansible-playbook haproxy.yml
-ansible-playbook nginx-reverse-proxy.yml
+#ansible-playbook haproxy.yml
+#ansible-playbook nginx-reverse-proxy.yml
